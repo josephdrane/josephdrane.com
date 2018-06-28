e8e14afb32e69ce3c9181bf383f6051ac0ee2f39
drane <drane@dranes-mbp.attlocal.net>
2018-06-28 01:37:22 -0500

    switched to gunicorn"

--- app.py
+++ app.py
@@ -53,4 +53,5 @@ def homePage():
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     # app.debug = True
-    app.run(host='0.0.0.0', port=8000)
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=9000)
